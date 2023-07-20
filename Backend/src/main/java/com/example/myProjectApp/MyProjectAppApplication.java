bae90cd58f2f554474d9c3122ed82774ef8b6ab6
Yaren Can <yarencanz@outlook.com>
2023-07-20 12:06:26 +0300

    Main page.

---
+++ Backend/src/main/java/com/example/myProjectApp/MyProjectAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.myProjectApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProjectAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyProjectAppApplication.class, args);
+	}
+
+}
