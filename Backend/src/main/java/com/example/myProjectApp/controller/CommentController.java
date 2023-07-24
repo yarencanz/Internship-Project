0a98372e87f0ecd425832e614ae6ef20bf7e9df4
Yaren Can <yarencanz@outlook.com>
2023-07-24 23:29:48 +0300

    Created CommentController.

---
+++ Backend/src/main/java/com/example/myProjectApp/controller/CommentController.java
@@ -0,0 +1,45 @@
+package com.example.myProjectApp.controller;
+
+import com.example.myProjectApp.entities.Comment;
+import com.example.myProjectApp.requests.CommentCreateRequest;
+import com.example.myProjectApp.requests.CommentUpdateRequest;
+import com.example.myProjectApp.services.CommentService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/comments")
+public class CommentController {
+    private CommentService commentService;
+
+    public CommentController(CommentService commentService){
+        this.commentService = commentService;
+    }
+
+    @GetMapping
+    public List<Comment> getAllComments(@RequestParam Optional<Long> userId, @RequestParam Optional<Long> postId){
+         return  commentService.getAllCommentsWithParam(userId,postId);
+    }
+
+    @PostMapping
+    public  Comment createOneComment(@RequestBody CommentCreateRequest request){
+        return  commentService.createOneComment(request);
+    }
+
+    @GetMapping("/{commentId}")
+    public Comment getOneComment(@PathVariable Long commentId){
+        return commentService.getOneCommentById(commentId);
+        }
+
+    @PutMapping("/{commentId}")
+    public Comment updateOneComment(@PathVariable Long commentId, @RequestBody CommentUpdateRequest request){
+        return commentService.updateOneCommentById(commentId, request);
+    }
+
+    @DeleteMapping("/{commentId}")
+    public  void deleteOneComment(@PathVariable Long commentId){
+        commentService.deleteOneCommentById(commentId);
+    }
+}
