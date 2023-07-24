b59df36af44e36a924be6dc3c80945ae480eccef
Yaren Can <yarencanz@outlook.com>
2023-07-24 23:32:51 +0300

    Refactor Repositories.

--- Backend/src/main/java/com/example/myProjectApp/repos/PostRepository.java
+++ Backend/src/main/java/com/example/myProjectApp/repos/PostRepository.java
@@ -3,5 +3,8 @@ package com.example.myProjectApp.repos;
 import com.example.myProjectApp.entities.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findByUserId(Long userId);
 }
