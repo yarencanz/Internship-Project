a52d7390d3ec51a7c04f598775c301694a22afa7
Yaren Can <yarencanz@outlook.com>
2023-07-24 23:30:02 +0300

    Created LikeController.

---
+++ Backend/src/main/java/com/example/myProjectApp/controller/LikeController.java
@@ -0,0 +1,39 @@
+package com.example.myProjectApp.controller;
+
+import com.example.myProjectApp.entities.Like;
+import com.example.myProjectApp.requests.LikeCreateRequest;
+import com.example.myProjectApp.services.LikeService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/likes")
+public class LikeController {
+
+    private LikeService likeService;
+
+    public LikeController(LikeService likeService){
+        this.likeService = likeService;
+    }
+
+    @GetMapping
+    public List<Like> getAllLikes(@RequestParam Optional<Long> userId, @RequestParam Optional<Long> postId){
+        return  likeService.getAllLikesWithParam(userId,postId);
+    }
+
+    @PostMapping
+    public Like createOneLike(@RequestBody LikeCreateRequest request){
+        return  likeService.createOneLike(request);
+    }
+    @GetMapping("/{likeId}")
+    public Like getOneLike(@PathVariable Long likeId){
+        return likeService.getOneLikeById(likeId);
+    }
+    @DeleteMapping("/{commentId}")
+    public  void deleteOneComment(@PathVariable Long likeId){
+        likeService.deleteOneLikeById(likeId);
+    }
+
+}
