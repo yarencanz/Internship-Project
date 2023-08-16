dbbd34ce2f0f7c728e217bb013002a418956e72f
Yaren Can <yarencanz@outlook.com>
2023-08-16 19:28:04 +0300

    Built Post component.

--- frontend/src/components/Post/Post.js
+++ frontend/src/components/Post/Post.js
@@ -1,41 +1,76 @@
-import React, {useState, useEffect} from "react";
-import ReactDom from "react-dom";
+import React from "react";
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import InsertCommentIcon from '@mui/icons-material/InsertComment';
 
-function Post(){
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [postList, setPostList] = useState([]);
 
-    useEffect(() => {
-        fetch("/posts")
-        .then(res => res.json())
-        .then(
-            (result) => {
-                setIsLoaded(true);
-                setPostList(result)
-            },
-            (error) => {
-                setIsLoaded(true);
-                setError(error);
-            }
-        )
-    }, [])
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
 
-    if(error){
-        return <div> Error !!!</div>;
-    }else if(!isLoaded){
-        return <div> Loading... </div>
-    }else{
-        return(
-            <ul>
-                {postList.map( post => (
-                    <li>
-                        {post.title} {post.text}
-                    </li>
-                ))}
-            </ul>
-        )
-    }
+
+function Post(props){
+  const {title, text} = props;
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return(
+    
+       <Card sx={{ width: 345 }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            R
+          </Avatar>
+        }
+        title={title}
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          {text}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+       
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <InsertCommentIcon />
+        </ExpandMore>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+         
+        </CardContent>
+      </Collapse>
+    </Card>
+    
+  )
 }
 
 export default Post;
