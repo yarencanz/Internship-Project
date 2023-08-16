a7a0c296c09a1e505107e761d2944e066c4955be
Yaren Can <yarencanz@outlook.com>
2023-08-16 22:59:04 +0300

    Refactor index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Post from '../src/components/Post/Post';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Post />
+    <App />
   </React.StrictMode>
 );
 
